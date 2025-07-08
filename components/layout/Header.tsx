0ede8633051afbeec6514ff91db6a9fac96919a1
markshallue <84836432+markshallue@users.noreply.github.com>
2025-07-08 12:20:06 +0200

    Add version number to header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,9 +4,10 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
-import { Box, Burger, Group, ThemeIcon, Title } from '@mantine/core';
+import { Badge, Box, Burger, Group, ThemeIcon, Title } from '@mantine/core';
 
 import { pages } from '@/config';
+import { version } from '@/package.json';
 
 import classes from './Header.module.css';
 
@@ -33,22 +34,25 @@ export function Header({ opened, toggle }: Props) {
 	return (
 		<header className={classes.header}>
 			<div className={classes.inner}>
-				<Group>
+				<Group wrap='nowrap'>
 					<Burger opened={opened} onClick={toggle} size='sm' hiddenFrom='sm' />
 					<Box component={Link} href='/' td='none' className={classes.title}>
-						<Group gap='sm' align='center'>
+						<Group gap='sm' align='center' wrap='nowrap'>
 							<ThemeIcon color='indigo' radius='md'>
 								<Image src='/calendar-event-white.svg' alt='Calendar icon' width={18} height={18} />
 							</ThemeIcon>
-							<Title order={3} fw={600} className={classes.titleText}>
+							<Title order={3} className={classes.titleText}>
 								Events Calendar
 							</Title>
+							<Badge variant='default' tt='none' ff='monospace' visibleFrom='md'>
+								v{version}
+							</Badge>
 						</Group>
 					</Box>
 				</Group>
 
-				<Group gap='xs'>
-					<Group gap='xs' visibleFrom='sm'>
+				<Group gap='xs' wrap='nowrap'>
+					<Group gap='xs' visibleFrom='sm' wrap='nowrap'>
 						{items}
 					</Group>
 
