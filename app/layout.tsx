47fec91c2f2c1d4d645af94b736f7b59dc7c320b
markshallue <84836432+markshallue@users.noreply.github.com>
2025-07-31 12:46:42 +0200

    Include mantine dates styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.layer.css';
+import '@mantine/dates/styles.layer.css';
 import '@mantine/carousel/styles.layer.css';
-import '@mantine/code-highlight/styles.css';
+import '@mantine/code-highlight/styles.layer.css';
 
 import React from 'react';
 import { Space_Grotesk } from 'next/font/google';
